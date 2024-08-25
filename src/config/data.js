6f9eb6241c51c4377fdcd700037b3d73f3003166
rutuja091 <gaikwadrutuja254@gmail.com>
2024-08-25 21:18:26 +0530

    changes in config

--- src/config/data.js
+++ src/config/data.js
@@ -6,35 +6,35 @@ const PRODUCTS = [
     {
         title : " Veggie Burger",
         description :"“A fusion of classic Indian flavors and the American classic burger.🍔”",
-        price : "₹ 150",
+        price : "₹ 120",
         imgUrl:"https://www.mccainindia.com/wp-content/uploads/2023/01/tandori-veggi-burger.jpg",
         isveg :true
         },
         {
            title : "Non Veg-Schezwan Chicken Pizza",
            description :"“fiery Schezwan sauce meets tender chicken atop a crispy crust🍕”",
-           price : "₹ 250",
+           price : "₹ 300",
            imgUrl:"https://static.tnn.in/thumb/msid-91549781,thumbsize-125986,width-1280,height-720,resizemode-75/91549781.jpg?quality=100",
            isveg :false
         },
         {
            title : "sandwich",
            description :"“ Vegetable sandwich is a type of vegetarian sandwich consisting of a vegetable filling between bread.”",
-           price : "₹ 299",
+           price : "₹ 150",
            imgUrl:"https://img.pikbest.com/origin/09/14/42/82ipIkbEsT2dS.jpg!w700wp ",
            isveg : true
         },
         {
            title : "Dum Biryani",
            description :"“The meat is marinated and cooked along with short-grained and fine rice.🍗”",
-           price : "₹ 850",
+           price : "₹ 750",
            imgUrl:"https://www.licious.in/blog/wp-content/uploads/2023/01/Shutterstock_2047827035-1024x683.jpg",
            isveg : false
         },
         {
            title : "Paneer",
            description :"“Paneer is a traditional Indian cheese made with just 2 ingredients - milk and an acidic ingredient. 😋”",
-           price : "₹ 120",
+           price : "₹ 260",
            imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA25N-7FVPa2-vstPOHFFUUj8s8Gjs707GGQ&s",
            isveg : true
      
@@ -42,7 +42,7 @@ const PRODUCTS = [
         {
               title : "Pav Bhaji",
               description :"“ a spiced mixture of mashed vegetables in a thick gravy served with bread.”",
-              price : "₹ 90",
+              price : "₹ 240",
               imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-MYN4FHWkyIS5c7nKOhWr2lc4DZs8Ki6G2w&s",
               isveg : true
         
@@ -50,14 +50,14 @@ const PRODUCTS = [
          {
            title : "Fish Fry",
            description :"“A fish fry is a meal containing battered or breaded fried fish.”",
-           price : "₹ 120",
+           price : "₹ 190",
            imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8yZa7g7e5TQeWDYX9cqSkxVMyxINHfGvyjw&s",
            isveg : false
         },
         {
            title : "chicken tikka",
            description :"“Chicken Tikka is a popular sub-continent dish where pieces of marinated chicken are cooked in a tandoor/oven.”",
-           price : "₹ 220",
+           price : "₹ 380",
            imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeVEbdSnZaU2hKSrj4FxPVGBR7NSXq_Z_7rQ&s",
            isveg : false
         },
